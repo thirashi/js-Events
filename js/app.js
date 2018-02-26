e57430762fd4ed2135036f17feab6598cc18f81f
Tyler Hirashima <tylerhirashima@Tylers-MacBook-Pro-5.local>
2018-02-25 15:30:46 -1000

    Finished Ex6

--- js/app.js
+++ js/app.js
@@ -58,6 +58,12 @@ function redFace(){
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
 
+function showPrice(){
+
+	document.getElementById('price').innerHTML='$5.55';
+
+}
+
 
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
